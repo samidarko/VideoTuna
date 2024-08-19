c95bd9165679dd7f15d83b32bf08fde69c4f9a12
Fang, Pengjun <138440893+ff2416@users.noreply.github.com>
2024-08-20 00:22:11 +0800

    Update inference_flux.sh

--- scripts/inference_flux.sh
+++ scripts/inference_flux.sh
@@ -2,8 +2,9 @@ export FLUX_SCHNELL='/disk2/pengjun/flux/FLUX.1-schnell/flux1-schnell.safetensor
 export FLUX_DEV='/disk2/pengjun/flux/FLUX.1-dev/flux1-dev.safetensors'
 export AE='/disk2/pengjun/flux/FLUX.1-schnell/ae.safetensors'
 model=flux-schnell #flux-schnell/flux-dev
-CUDA_VISIBLE_DEVICES=1 python -m flux \
+python -m flux/cli.py \
   --name $model \
   --height 768 --width 1360 \
   --prompt "a girl is dancing" \
   --device cuda \
+  --output_dir output
