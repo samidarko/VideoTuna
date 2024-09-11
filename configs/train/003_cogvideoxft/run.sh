f0e2d76b2f114e87051a588ecca8feeb55337639
haoyu <2928826539@qq.com>
2024-09-12 01:42:06 +0800

    Validation of training code.

--- configs/train/003_cogvideoxft/run.sh
+++ configs/train/003_cogvideoxft/run.sh
@@ -5,18 +5,18 @@ export TOKENIZERS_PARALLELISM=false
 COGCKPT="/home/rliuay/haoyu/research/VideoTuna/cogVideo/CogVideoX-2b-sat/transformer/1000/mp_rank_00_model_states.pt"
 
 # exp settings
-EXPNAME="correctness"                            # experiment name 
+EXPNAME="correctness"                              # experiment name 
 CONFIG='configs/train/003_cogvideoxft/config.yaml' # experiment config 
-RESROOT="results/cogvideo_train"                               # experiment saving directory
+RESROOT="results/cogvideo_train"                   # experiment saving directory
 
 # run
 current_time=$(date +%Y%m%d%H%M%S)
 python scripts/train.py \
--t \
---name "$current_time"_$EXPNAME \
---base $CONFIG \
---logdir $RESROOT \
---ckpt $COGCKPT \
---devices '0,' \
-lightning.trainer.num_nodes=1 \
---auto_resume True
+    -t \
+    --name "$current_time"_$EXPNAME \
+    --base $CONFIG \
+    --logdir $RESROOT \
+    --ckpt $COGCKPT \
+    --devices '0,' \
+    lightning.trainer.num_nodes=1 \
+    --auto_resume True
