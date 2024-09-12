ca2df1cc4cf2eaaf48870cdfbffbb289c6fd43fb
YingqingHe <heyingqing0023@gmail.com>
2024-09-12 20:52:24 +0800

    minor fix

--- shscripts/inference_flux_dev.sh
+++ shscripts/inference_flux_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python inference_flux_dev.py \
+python scripts/inference_flux_dev.py \
 --prompt "A cat holding a sign that says hello world" \
 --out_path './image.png' \
 --width 1360 \
--- shscripts/inference_flux_schnell.sh
+++ shscripts/inference_flux_schnell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python inference_flux_schnell.py \
+python scripts/inference_flux_schnell.py \
 --prompt "A cat holding a sign that says hello world" \
 --out_path './image.png' \
 --width 1360 \
