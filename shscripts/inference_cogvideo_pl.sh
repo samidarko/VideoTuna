b532ea0b0f38d3b9f3f5525f01caf9013ce0ca15
haoyu <2928826539@qq.com>
2024-09-14 18:14:48 +0800

    fix inference scripts arguments.

--- shscripts/inference_cogvideo_pl.sh
+++ shscripts/inference_cogvideo_pl.sh
@@ -1,29 +1,19 @@
+export CUDA_VISIBLE_DEVICES=0
+savedir="results/cogvideo/t2v_pl"
+prompt_file="inputs/t2v/cogvideo/elon_musk_video/labels/1.txt"
 environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
+
 export environs
 
-
-savedir="results/cogvideo/t2v_pl/125"
-prompt_file="src/sat/configs/test.txt"
 ckpt_path="cogVideo/CogVideoX-2b-sat/transformer/1000/mp_rank_00_model_states.pt"
-
-# orig arg format
+# ckpt_path="results/cogvideo_train/20240913160358_correctness/checkpoints/epoch=0005-step=000600.ckpt"
 python scripts/inference_cogvideo.py \
-    --base configs/inference/cogvideo_t2v_pl.yaml  \
-    --input-file $prompt_file \
-    --load $ckpt_path \
-    --sampling-num-frames 13 \
-    --sampling-fps 8 \
-    --force-inference \
-    --seed 123
-
-# vc2 args format
-# python scripts/inference_cogvideo.py \
-#     --base configs/inference/cogvideo_t2v_pl.yaml  \
-#     --savedir $savedir \
-#     --prompt_file $prompt_file \
-#     --load "cogVideo/CogVideoX-2b-sat/transformer/1000/mp_rank_00_model_states.pt" \
-#     --bs 1 --height 480 --width 720 \
-#     --frames 13 \
-#     --savefps 8 \
-#     --seed 125
+    --config "configs/train/005_cogvideoxft/config.yaml" \
+    --savedir $savedir \
+    --prompt_file $prompt_file \
+    --ckpt_path $ckpt_path  \
+    --bs 1 --height 480 --width 720 \
+    --frames 13 \
+    --savefps 8 \
+    --seed 125
 
