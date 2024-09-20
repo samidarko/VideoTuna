89c06f00c5224afd812aef668243bc2fee365f8b
YingqingHe <heyingqing0023@gmail.com>
2024-09-20 16:58:51 +0800

    update script

--- shscripts/inference_vc2_t2v_320x512.sh
+++ shscripts/inference_vc2_t2v_320x512.sh
@@ -1,15 +1,15 @@
 ckpt='checkpoints/videocrafter/t2v_v2_512/model.ckpt'
-# config='configs/inference/inference_t2v_512_v2.0.yaml'
 config='configs/inference/vc2_t2v_512.yaml'
 prompt_file="inputs/t2v/prompts.txt"
-savedir="results/t2v/videocrafter2"
+current_time=$(date +%Y%m%d%H%M%S)
+savedir="results/t2v/$current_time-videocrafter2"
 
 python3 scripts/inference.py \
---ckpt_path $ckpt \
---config $config \
---prompt_file $prompt_file \
---savedir $savedir \
---bs 1 --height 320 --width 512 \
---fps 28 \
---seed 123
+    --ckpt_path $ckpt \
+    --config $config \
+    --prompt_file $prompt_file \
+    --savedir $savedir \
+    --bs 1 --height 320 --width 512 \
+    --fps 28 \
+    --seed 123
 
