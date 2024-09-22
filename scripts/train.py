689ecec5b54c246a406c0f2222a721c1763bc6de
SHAO Guocheng <1162524575@qq.com>
2024-09-22 23:46:54 +0800

    Revert "Revert "revise the code to adapt to pytorch lightning 2.4 ver""

--- scripts/train.py
+++ scripts/train.py
@@ -4,8 +4,7 @@ from transformers import logging as transf_logging
 
 import torch
 import pytorch_lightning as pl
-from pytorch_lightning import seed_everything
-from pytorch_lightning.trainer import Trainer
+from pytorch_lightning import seed_everything, Trainer
 from pytorch_lightning.cli import LightningCLI
 
 # sys.path.insert(1, os.path.join(sys.path[0], '..'))
@@ -22,9 +21,11 @@ def get_parser(**parser_kwargs):
     parser.add_argument("--seed", "-s", type=int, default=20230211, help="seed for seed_everything")
     parser.add_argument("--name", "-n", type=str, default="", help="experiment name, as saving folder")
 
-    parser.add_argument("--base", "-b", nargs="*", metavar="base_config.yaml", help="paths to base configs. Loaded from left-to-right. "
-                            "Parameters can be overwritten or added with command-line options of the form `--key value`.", default=list())
-    
+    parser.add_argument("--base", "-b", nargs="*", metavar="base_config.yaml",
+                        help="paths to base configs. Loaded from left-to-right. "
+                             "Parameters can be overwritten or added with command-line options of the form `--key value`.",
+                        default=list())
+
     parser.add_argument("--train", "-t", action='store_true', default=False, help='train')
     parser.add_argument("--val", "-v", action='store_true', default=False, help='val')
     parser.add_argument("--test", action='store_true', default=False, help='test')
@@ -37,16 +38,15 @@ def get_parser(**parser_kwargs):
     parser.add_argument("--ckpt", type=str, default=None, help="pretrained current model checkpoint")
     parser.add_argument("--lorackpt", type=str, default=None, help="pretrained current model checkpoint")
     return parser
-    
+
+
 def get_nondefault_trainer_args(args):
     parser = argparse.ArgumentParser()
-    try:
-        parser = Trainer.add_argparse_args(parser)
-    except:
-        parser = add_trainer_args_to_parser(Trainer, parser)
+    parser = add_trainer_args_to_parser(Trainer, parser)
 
     default_trainer_args = parser.parse_args([])
     return sorted(k for k in vars(default_trainer_args) if getattr(args, k) != getattr(default_trainer_args, k))
+
 
 if __name__ == "__main__":
     now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
@@ -56,16 +56,11 @@ if __name__ == "__main__":
         num_rank = int(os.environ.get('WORLD_SIZE'))
     except:
         local_rank, global_rank, num_rank = 0, 0, 1
-    #print(f'local_rank: {local_rank} | global_rank:{global_rank} | num_rank:{num_rank}')
 
     parser = get_parser()
     ## Extends existing argparse by default Trainer attributes
-    
-    try:
-        parser = Trainer.add_argparse_args(parser)
-    except:
-        parser = add_trainer_args_to_parser(Trainer, parser)
-    
+    parser = add_trainer_args_to_parser(Trainer, parser)
+
     args, unknown = parser.parse_known_args()
     ## disable transformer warning
     transf_logging.set_verbosity_error()
@@ -82,43 +77,41 @@ if __name__ == "__main__":
         config["model"]["pretrained_checkpoint"] = args.ckpt
     if args.lorackpt is not None:
         config["model"]["params"]["lora_args"]["lora_ckpt"] = args.lorackpt
-    
+
     lightning_config = config.pop("lightning", OmegaConf.create())
-    trainer_config = lightning_config.get("trainer", OmegaConf.create()) 
+    trainer_config = lightning_config.get("trainer", OmegaConf.create())
 
     ## setup workspace directories
     workdir, ckptdir, cfgdir, loginfo = init_workspace(args.name, args.logdir, config, lightning_config, global_rank)
-    logger = set_logger(logfile=os.path.join(loginfo, 'log_%d:%s.txt'%(global_rank, now)))
-    logger.info("@lightning version: %s [>=1.8 required]"%(pl.__version__))  
+    logger = set_logger(logfile=os.path.join(loginfo, 'log_%d:%s.txt' % (global_rank, now)))
+    logger.info("@lightning version: %s [>=2.0 required]" % pl.__version__)
 
     ## MODEL CONFIG >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    logger.info("***** Configing Model *****")
+    logger.info("***** Configuring Model *****")
     config.model.params.logdir = workdir
-    
-    
+
     model = instantiate_from_config(config.model)
-    # import pdb; pdb.set_trace()
     if args.auto_resume:
         ## the saved checkpoint must be: full-info checkpoint
         resume_ckpt_path = get_autoresume_path(workdir)
         if resume_ckpt_path is not None:
             args.resume_from_checkpoint = resume_ckpt_path
-            logger.info("Resuming from checkpoint: %s"%args.resume_from_checkpoint)
+            logger.info("Resuming from checkpoint: %s" % args.resume_from_checkpoint)
             ## just in case train empy parameters only
             if check_config_attribute(config.model.params, 'empty_params_only'):
                 _, model.empty_paras = get_empty_params_comparedwith_sd(model, config.model)
         else:
             model = load_checkpoints(model, config.model)
-            logger.warning("Auto-resuming skipped as No checkpoit found!")
+            logger.warning("Auto-resuming skipped as No checkpoint found!")
     else:
         model = load_checkpoints(model, config.model)
-    if len(model.lora_args)!=0:
+    if len(model.lora_args) != 0:
         model.inject_lora()
     ## update trainer config
     for k in get_nondefault_trainer_args(args):
         trainer_config[k] = getattr(args, k)
-    
-    print(trainer_config)
+
+    print(f'trainer_config: {trainer_config}')
     num_nodes = trainer_config.num_nodes
     ngpu_per_node = trainer_config.devices
     logger.info(f"Running on {num_rank}={num_nodes}x{ngpu_per_node} GPUs")
@@ -131,17 +124,15 @@ if __name__ == "__main__":
     else:
         model.learning_rate = base_lr
 
-
     ## DATA CONFIG >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    logger.info("***** Configing Data *****")
+    logger.info("***** Configuring Data *****")
     data = instantiate_from_config(config.data)
     data.setup()
     for k in data.datasets:
         logger.info(f"{k}, {data.datasets[k].__class__.__name__}, {len(data.datasets[k])}")
 
-
     ## TRAINER CONFIG >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    logger.info("***** Configing Trainer *****")
+    logger.info("***** Configuring Trainer *****")
     if "accelerator" not in trainer_config:
         trainer_config["accelerator"] = "gpu"
 
@@ -150,7 +141,7 @@ if __name__ == "__main__":
     trainer_kwargs["num_sanity_val_steps"] = 0
     logger_cfg = get_trainer_logger(lightning_config, workdir, args.debug)
     trainer_kwargs["logger"] = instantiate_from_config(logger_cfg)
-    print(trainer_kwargs['logger'].save_dir)
+    print(f"logger save_dir: {trainer_kwargs['logger'].save_dir}")
     ## setup callbacks
     callbacks_cfg = get_trainer_callbacks(lightning_config, config, workdir, ckptdir, logger)
     callbacks_cfg['image_logger']['params']['save_dir'] = workdir
@@ -161,19 +152,21 @@ if __name__ == "__main__":
 
     ## trainer config: others
     if "train" in config.data.params and config.data.params.train.target == "lvdm.data.hdvila.HDVila" or \
-        ("validation" in config.data.params and config.data.params.validation.target == "lvdm.data.hdvila.HDVila"):
+            ("validation" in config.data.params and config.data.params.validation.target == "lvdm.data.hdvila.HDVila"):
         trainer_kwargs['replace_sampler_ddp'] = False
 
     ## for debug
-    #trainer_kwargs["fast_dev_run"] = 10
-    #trainer_kwargs["limit_train_batches"] = 1./32
-    #trainer_kwargs["limit_val_batches"] = 0.01
-    #trainer_kwargs["val_check_interval"] = 20  #float: epoch ratio | integer: batch num
-    
+    # trainer_kwargs["fast_dev_run"] = 10
+    # trainer_kwargs["limit_train_batches"] = 1./32
+    # trainer_kwargs["limit_val_batches"] = 0.01
+    # trainer_kwargs["val_check_interval"] = 20  #float: epoch ratio | integer: batch num
+
     # merge args for trainer
-    trainer_args = argparse.Namespace(**trainer_config)
-    trainer = Trainer.from_argparse_args(trainer_args, **trainer_kwargs)
-    print(trainer_args,trainer_kwargs)
+    print(f'trainer_kwargs: {trainer_kwargs}')
+    trainer = Trainer(**trainer_config, **trainer_kwargs)
+
+
+
     ## allow checkpointing via USR1
     def melk(*args, **kwargs):
         ## run all checkpoint hooks
@@ -182,32 +175,34 @@ if __name__ == "__main__":
             ckpt_path = os.path.join(ckptdir, "last_summoning.ckpt")
             trainer.save_checkpoint(ckpt_path)
 
+
     def divein(*args, **kwargs):
         if trainer.global_rank == 0:
-            import pudb;
+            import pudb
             pudb.set_trace()
 
+
     import signal
+
     signal.signal(signal.SIGUSR1, melk)
     signal.signal(signal.SIGUSR2, divein)
 
-    ## Running LOOP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## Running LOOP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     logger.info("***** Running the Loop *****")
+
     if args.train:
         try:
-            if "strategy" in lightning_config:
-                logger.info("<Training in DeepSpeed Mode>")
-                ## deepspeed
-                with torch.cuda.amp.autocast():
-                    trainer.fit(model, data)
-            else:
-                logger.info("<Training in DDPShare Mode>")
-                ## ddpshare
-                trainer.fit(model, data)
-        except Exception:
-            #melk()
-            raise
+            # Strategy is automatically managed, no need to manually check it here
+            logger.info(f"<Training in {trainer.strategy.__class__.__name__} Mode>")
+            trainer.fit(model, data)
+        except Exception as e:
+            logger.error(f"Training failed: {str(e)}")
+            raise  # Raising the error will help in debugging
+
     if args.val:
+        # Directly call validation
         trainer.validate(model, data)
+
+    # Ensure test runs either after training finishes or if explicitly requested
     if args.test or not trainer.interrupted:
         trainer.test(model, data)
