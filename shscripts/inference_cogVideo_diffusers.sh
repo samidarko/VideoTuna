842dc33cc507871928d1bb248b2d0a38578a1725
YingqingHe <heyingqing0023@gmail.com>
2024-09-22 18:18:23 +0800

    style: change the default ckpt location of cogvideox

--- shscripts/inference_cogVideo_diffusers.sh
+++ shscripts/inference_cogVideo_diffusers.sh
@@ -1,6 +1,6 @@
 python scripts/inference_cogVideo_diffusers.py \
 --prompt "A cat playing with a ball" \
---model_path cogVideo/CogVideoX-2b \
+--model_path checkpoints/cogvideo/CogVideoX-2b \
 --output_path output.mp4 \
 --num_inference_steps 50 \
 --guidance_scale 3.5 \
