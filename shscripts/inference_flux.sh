45591e33960ed5d250dfba91c1b41055be3346ea
Fang, Pengjun <138440893+ff2416@users.noreply.github.com>
2024-10-31 19:34:52 +0800

    Update model path

--- shscripts/inference_flux.sh
+++ shscripts/inference_flux.sh
@@ -1,6 +1,6 @@
-export FLUX_SCHNELL='/disk2/pengjun/flux/FLUX.1-schnell/flux1-schnell.safetensors'
-export FLUX_DEV='/disk2/pengjun/flux/FLUX.1-dev/flux1-dev.safetensors'
-export AE='/disk2/pengjun/flux/FLUX.1-schnell/ae.safetensors'
+export FLUX_SCHNELL='checkpoints/FLUX.1-schnell/flux1-schnell.safetensors'
+export FLUX_DEV='checkpoints/FLUX.1-dev/flux1-dev.safetensors'
+export AE='checkpoints/FLUX.1-schnell/ae.safetensors'
 model=flux-schnell #flux-schnell/flux-dev
 python -m src/flux/cli.py \
   --name $model \
