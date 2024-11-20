d5c784f432eccc43b20c9af47af8b27ee046a246
LI, Jiajun <jliiw@connect.ust.hk>
2024-11-20 17:50:06 +0800

    code update for cogvideox1.5

---
+++ shscripts/inference_cogVideox1.5_5b_i2v.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
+
+run_cmd="$environs python scripts/inference_cogVideo_sat.py --base configs/005_cogvideox1.5/cogvideox1.5_5b_i2v.yaml configs/005_cogvideox1.5/inference1.5_5b_i2v.yaml --seed $RANDOM"
+
+echo ${run_cmd}
+eval ${run_cmd}
+
+echo "DONE on `hostname`"
