4e176fc793e7d10dec1e6815373a7a471fec435f
litwellchi <litwellchi@gmail.com>
2024-12-09 14:36:43 +0800

    add docker file

---
+++ docker/start_docker.sh
@@ -0,0 +1,6 @@
+cd ./docker
+docker build -t videotuna:v1.0 .
+docker run --gpus all -it videotuna:v1.0 /bin/bash 
+
+# if you want to use your local path:
+docker run  -v /path/to/VideoTuna/:/content/local_VideoTuna --gpus all -it videotuna:v1.0 /bin/bash
